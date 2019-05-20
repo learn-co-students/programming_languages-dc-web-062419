d176aecb917b112d5bd9b0b1d02097e5861b9c38
Trevahr Robertson <trevahr@yahoo.com>
2019-05-20 23:36:29 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,60 @@
+require "pry"
+
+
+pl =     {
+      :oo => {
+        :ruby => {
+          :type => "interpreted"
+        },
+        :javascript => {
+          :type => "interpreted"
+        },
+        :python => {
+          :type => "interpreted"
+        },
+        :java => {
+          :type => "compiled"
+        }
+      },
+      :functional => {
+        :clojure => {
+          :type => "compiled"
+        },
+        :erlang => {
+          :type => "compiled"
+        },
+        :scala => {
+          :type => "compiled"
+        },
+        :javascript => {
+          :type => "interpreted"
+        }
+        }
+        }
+
+
+
 def reformat_languages(languages)
-  # your code here
+  rl = {}
+  language_keys = {}
+  languages.map do |style, language_data|
+      
+      language_data.map do |language_name, attributes|
+        
+          rl[language_name]= attributes
+          
+      end
+  end
+  
+  rl
+  
 end
+
+
+
+
+
+
+
+
+
