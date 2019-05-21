93cd7b46ab5fce1626bf11c18cf0de0cd6bf5924
Trevahr Robertson <trevahr@yahoo.com>
2019-05-21 05:49:57 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -33,23 +33,23 @@ pl =     {
         }
 
 
-
+=begin
 def reformat_languages(languages)
   rl = {}
   language_keys = {}
   languages.map do |style, language_data|
     
-      
       language_data.map do |language_name, attributes|
         
           rl[language_name] = attributes
-          
+       
+            
           if rl[language_name][:style].nil? 
-            rl[language_name][:style]= [style]
-          else
-            rl[language_name][:style].push(style)
-          
-        end
+            rl[language_name][:style] = [style]
+          else 
+            rl[language_name][:styl].push("style")
+          end
+        #end
       end
   
   end
@@ -57,9 +57,66 @@ def reformat_languages(languages)
   rl
   
 end
+=end
+ 
+ 
 
+def r
+  rl = {}
+  language_keys = {}
 
+  languages    =  {
+      :oo => {
+        :ruby => {
+          :type => "interpreted"
+        },
+        :javascript => {
+          :type => "interpreted"
+          
+        },
+        :python => {
+          :type => "interpreted"
+        },
+        :java => {
+          :type => "compiled"
+        }
+      },
+      :functional => {
+        :clojure => {
+          :type => "compiled"
+        },
+        :erlang => {
+          :type => "compiled"
+        },
+        :scala => {
+          :type => "compiled"
+        },
+        :javascript => {
+          :type => "interpreted",
+          :style => ["empty"]
+        }
+        }
+        }
+  languages.map do |style, language_data|
+    
+      language_data.map do |language_name, attributes|
+        
 
+        #attributes.map do |k, v|
+
+         # rl[language_name][attributes] = {k => v}   
+          rl[language_name] = attributes
+          if rl[language_name][:style].nil?
+           # rl[language_name][:style] = [style]
+          end
+          
+            #if rl[language_name] == :javascript
+            #rl[language_name][:style] = []
+          #end
+      end
+    end
+  rl
+  end
 
 
 
