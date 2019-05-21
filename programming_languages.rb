d9ad7a87b307536df2c5cd932a84d3b5a550993a
Trevahr Robertson <trevahr@yahoo.com>
2019-05-21 20:32:32 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 
-pl =     {
+languages =     {
       :oo => {
         :ruby => {
           :type => "interpreted"
@@ -31,95 +31,50 @@ pl =     {
         }
         }
         }
-
-
-=begin
+        
+        
+        
+        
+        
+        
+        
 def reformat_languages(languages)
   rl = {}
-  language_keys = {}
+  
+
+
   languages.map do |style, language_data|
     
-      language_data.map do |language_name, attributes|
-        
-          rl[language_name] = attributes
-       
-            
-          if rl[language_name][:style].nil? 
-            rl[language_name][:style] = [style]
-          else 
-            rl[language_name][:styl].push("style")
-          end
-        #end
-      end
-  
-  end
-  
-  rl
-  
-end
-=end
- 
- 
-
-def r
-  rl = {}
-  language_keys = {}
-
-  languages    =  {
-      :oo => {
-        :ruby => {
-          :type => "interpreted"
-        },
-        :javascript => {
-          :type => "interpreted"
-          
-        },
-        :python => {
-          :type => "interpreted"
-        },
-        :java => {
-          :type => "compiled"
-        }
-      },
-      :functional => {
-        :clojure => {
-          :type => "compiled"
-        },
-        :erlang => {
-          :type => "compiled"
-        },
-        :scala => {
-          :type => "compiled"
-        },
-        :javascript => {
-          :type => "interpreted",
-          :style => ["empty"]
-        }
-        }
-        }
-  languages.map do |style, language_data|
     
       language_data.map do |language_name, attributes|
         
 
-        #attributes.map do |k, v|
+        attributes.map do |k, v|
 
          # rl[language_name][attributes] = {k => v}   
-          rl[language_name] = attributes
-          if rl[language_name][:style].nil?
-           # rl[language_name][:style] = [style]
-          end
+          rl[language_name] = {k => v}
           
+            #rl[language_name][k] = v
+          if 
+          if rl[language_name][:style].nil?
+           rl[language_name][:style] = []
+          end
+          #binding.pry
+          
+          end
+        rl[language_name][:style] << style
+        end
+          #rl[language_name][:style] << style
             #if rl[language_name] == :javascript
             #rl[language_name][:style] = []
-          #end
-      end
+        
+      #end
     end
   rl
-  end
 
-
-
-
-
-
+  end  
+        
+        
+        
+        
+        reformat_languages(languages)
