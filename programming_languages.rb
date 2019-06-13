f15407e9a325860aad6cb5d93d646d57b4487a39
Michelle Lam <shelly2.lam@gmail.com>
2019-06-12 22:47:05 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_languages = {}
+  languages.each do |style, language|
+    language.each do |name, attributes|
+    new_languages[name] ||= attributes
+    new_languages[name][:style]=[]
+    new_languages[name][:style]<<style
+    end
+  end
+  new_languages[:javascript][:style].unshift(:oo)
+  new_languages
+  #binding.pry
 end
+
+
+
+
