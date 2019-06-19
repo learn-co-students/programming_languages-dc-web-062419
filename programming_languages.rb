2e4c620a0d967215e88c8e000d000be1223816be
Ursula Miller <ursula.miller@gmail.com>
2019-06-19 15:14:34 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,89 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  temp_array = []
+  the_two_styles = []
+  array_languages = []
+  hash_of_styles = {}
+  languages.each do |style, language_data|
+    the_two_styles << style
+    language_data.each do |programming_langauge, data|
+      hash_of_styles[programming_langauge] = style
+#        array_languages << programming_langauge
+        new_hash[programming_langauge] = data 
+      new_hash[programming_langauge][:style] = []
+    end
+  end
+    languages.each do |style, language_data|
+      language_data.each do |programming_langauge, data|
+#        hash_of_styles[programming_langauge] = style
+#          new_hash[programming_langauge] = data 
+        new_hash[programming_langauge][:style] << style
+      end
+    end
+  puts temp_array
+#  puts "This is an array of languages#{array_languages}"
+  puts "but this is the new hash #{new_hash}"
+  return new_hash
 end
+
+
+
+
+def cats(languages)
+    new_hash = {}
+    languages.each do |style, language_data|
+      language_data.each do |language, data|
+        new_hash[language] = data
+        new_hash[language][:style] = []
+#        new_hash[language][:style] << style
+      end
+    end
+        languages.each do |style, language_data|
+      language_data.each do |language, data|
+#        new_hash[language] = data
+#        new_hash[language][:style] = []
+        new_hash[language][:style] << style
+      end
+    end
+  puts "MEEEEEEEEEEEEEOWWWWW"
+  puts "But what you really wantto know is what #{new_hash[:javascript][:style]} is"
+end
+
+
+
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
+cats(languages)
+
+reformat_languages(languages)
